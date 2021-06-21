0b14c4d32600e246f529b1ac05e8608649904ead
Alpaslan Dulkadir <alpaslandulkadir@gmail.com>
2021-06-21 21:22:40 +0300

    Initial commit

---
+++ ShopApp.Core.Webapi/Data/ShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Core.Webapi.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.Core.Webapi.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ShopApp.Core.Webapi/Entity/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.Core.Webapi.Entity
+{
+    public class Product
+    {
+    }
+}
